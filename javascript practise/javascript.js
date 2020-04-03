c1173aaf323ab3c2de2d48a1240239fc84da6f71
Leon Carnagie <leonfross@gmail.com>
2020-04-02 20:24:50 -0700

    adds files

---
+++ javascript practise/javascript.js
@@ -0,0 +1,8 @@
+
+/ When the boxGrow button is clicked... 
+$("#growBtn").on("click", function() {
+	// Increase the size of the box.
+	$("#box").style({height:"+=250px", width:"+=250px"}, "fast");
+})
+
+ 
